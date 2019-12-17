71ab8294160c393f14f0ef807985fde1d44d834c
Tasio <AgiBla98@gmail.com>
2019-12-17 22:11:17 +0200

    Visual changes

--- app/src/main/java/cs/ut/ee/fileencryption/SettingsActivity.kt
+++ app/src/main/java/cs/ut/ee/fileencryption/SettingsActivity.kt
@@ -2,10 +2,18 @@ package cs.ut.ee.fileencryption
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        encryptButton.setOnClickListener() {
+            val db = LocalDbClient.getDatabase(this)
+            db?.clearAllTables()
+
+            finish()
+        }
     }
 }
